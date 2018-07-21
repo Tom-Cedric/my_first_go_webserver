fef8cb253f5ece7fa3b797ec481e5be7535cb144
Tom-Cedric <tom-cedric.gottschlich@gmx.net>
2018-07-21 20:40:55 +0200

    First push

---
+++ forum/templates/temp_footer.php
@@ -0,0 +1,3 @@
+<div class="container">
+    <span class="text-muted"><small>Copyright by Th3 H3ll0 F0rum | 2018</small></span>
+</div>
---
+++ forum/templates/temp_forum.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <div class="card-header">
+        Foren
+    </div>
+    <div class="card-block forums-list">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <img class="forum-img" src="" />
+                Test Titel
+            </li>
+        </ul>
+    </div>
+</div>
