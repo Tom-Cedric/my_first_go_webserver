40b2fa557b4c542b9782263b2e4c1e0898bc4416
Tom-Cedric <tom-cedric.gottschlich@gmx.net>
2018-07-25 18:52:11 +0200

    Delete wrong files

--- forum/templates/forum_sites/forums_index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: DevCAAAKe
- * Date: 22.07.2018
- * Time: 08:37
- */
-
-echo($_GET['f']);
--- forum/templates/temp_footer.php
+++
@@ -1,3 +0,0 @@
-<div class="container">
-    <span class="text-muted"><small>Copyright by Th3 H3ll0 F0rum | 2018</small></span>
-</div>
